92bbf4c5328c3cec515e282f7e70e6ee44cefdb0
Christie Finnie <christie.finnie@gmail.com>
2019-11-23 12:43:06 -0800

    Add product items as rewards

--- seed_fun_land.rb
+++ seed_fun_land.rb
@@ -28,22 +28,24 @@ def make_guests_seed(number_guests = 250)
 end
 
 
-def make_rewards_seed(number_rewards = 3)
+def make_rewards_seed(number_rewards = 75)
   # Remove old file
     File.delete("rewards_seed.txt") if File.exist?("rewards_seed.txt")
   
   # Add reward items and make sure teh names are unique
   used_names = []
   number_rewards.to_i.times do |i| 
-    game_pick = Random.rand(2)
+    item_source_pick = Random.rand(3)
       
-    case game_pick
+    case item_source_pick
     when 0
     #dummy_name = Faker::Name.name      #=> "Christophe Bartell"
     #Faker::Commerce.product_name
       dummy_name = Faker::Games::Zelda.item
     when 1
       dummy_name = Faker::Games::Dota.item
+    when 2
+      dummy_name = Faker::Commerce.product_name
     end
 
     if used_names.include?(dummy_name) 
@@ -52,7 +54,7 @@ def make_rewards_seed(number_rewards = 3)
 
     dummy_tokens = Random.rand(5000)
     is_available = Random.rand(2) == 1
-    puts is_available
+    #puts is_available
     #puts dummy_name 
     #puts dummy_tokens
     insert_cmd = "INSERT INTO rewards (item_name, tokens, available) VALUES ('#{dummy_name}', #{dummy_tokens}, #{is_available});\n"
@@ -62,11 +64,17 @@ def make_rewards_seed(number_rewards = 3)
       f.write(insert_cmd)
     end
   
-    end
+  end # number_reward do
   
+  # Check if there's enough items
+  if used_names.length < number_rewards
+    puts "Unable to generate enough items..."
   end
+end # make_rewards_seed
 
 
 make_guests_seed
 make_rewards_seed
+
+
 

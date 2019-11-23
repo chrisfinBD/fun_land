4e40d72a25f2e1f4350d04dbf2c21ac7c1a2702d
Christie Finnie <christie.finnie@gmail.com>
2019-11-23 12:01:35 -0800

    Add new project files. Incomplete. Only adds one guest.

---
+++ seed_fun_land.rb
@@ -0,0 +1,36 @@
+# Christie Finnie
+# Due: 2019-11-23
+#
+# Ruby seed app for Fun Land game venue rewards and guests
+#
+require 'faker'
+
+def make_guests_seed(number_guests = 3)
+# Remove old file
+  File.delete("guests_seed.txt") if File.exist?("guests_seed.txt")
+
+# Add guests 
+  puts number_guests.class
+  number_guests.to_i.times do |i| 
+    puts i
+    dummy_name = Faker::Name.name      #=> "Christophe Bartell"
+    dummy_tokens = Random.rand(10000)
+puts dummy_name 
+puts dummy_tokens
+    insert_cmd = "INSERT INTO guests (name, tokens) VALUES (#{dummy_name}, #{dummy_tokens});"
+
+  # Write to file
+    File.open("guests_seed.txt", "w+") do |f|
+      f.write(insert_cmd)
+    end
+
+  end
+
+end
+
+
+make_guests_seed
+
+3.times do |i|
+  puts i
+end
